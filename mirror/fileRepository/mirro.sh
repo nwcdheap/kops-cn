9410d542859ada0ad35c85d86aa1439386d343b8
Pahud Hsieh <pahudnet@gmail.com>
2018-10-30 12:49:52 +0800

    init commit for fileRepo mirror script

---
+++ mirror/fileRepository/mirro.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+KOPS_VERSION='1.10.0'
+
+KUBERNETES_ASSETS=(
+  release/v1.10.3/
+  network-plugins/
+)
+
+mirror_kubernetes_release(){
+    for asset in "${KUBERNETES_ASSETS[@]}"; do
+        echo $asset
+        if [ ! -d ./kubernetes-release/$asset ]; then
+                mkdir -p ./kubernetes-release/$asset
+        fi
+        echo gsutil rsync -d -r gs://kubernetes-release/$asset ./kubernetes-release/$asset
+        gsutil -m rsync -d -r gs://kubernetes-release/$asset ./kubernetes-release/$asset
+    done
+}
+
+sync_release_to_s3(){
+    aws --profile=bjs s3 sync ./kubernetes-release/ s3://kubernetes-release/ --acl public-read
+}
+
+mirror_kubeupv2(){
+    TMPDIR="/tmp/kops/$KOPS_VERSION"
+    for platform in linux darwin
+    do
+        if [[ ! -d $TMPDIR/$platform/amd64 ]]; then
+        mkdir -p $TMPDIR/$platform/amd64
+        fi
+        if [[ ! -d $TMPDIR/images ]]; then
+        mkdir -p $TMPDIR/images
+        fi
+    done
+
+
+    for i in nodeup utils.tar.gz kops
+    do
+    for platform in linux darwin
+    do
+        wget -c https://kubeupv2.s3.amazonaws.com/kops/$KOPS_VERSION/$platform/amd64/$i -O $TMPDIR/$platform/amd64/$i
+        wget -c https://kubeupv2.s3.amazonaws.com/kops/$KOPS_VERSION/$platform/amd64/$i.sha1 -O $TMPDIR/$platform/amd64/$i.sha1
+    done
+    done
+
+    for platform in linux darwin
+    do
+    aws --profile bjs s3 sync $TMPDIR/$platform/amd64/ s3://kubeupv2/kops/$KOPS_VERSION/$platform/amd64/ --acl public-read
+    done
+
+    p=protokube.tar.gz
+    wget -c https://kubeupv2.s3.amazonaws.com/kops/$KOPS_VERSION/images/$p -O $TMPDIR/images/$p
+    wget -c https://kubeupv2.s3.amazonaws.com/kops/$KOPS_VERSION/images/$p.sha1 -O $TMPDIR/images/$p.sha1
+    aws --profile bjs s3 sync $TMPDIR/images/ s3://kubeupv2/kops/$KOPS_VERSION/images/ --acl public-read
+}
+
+
+
+mirror_fileRepo(){
+    aws --profile bjs s3 sync s3://kubernetes-release/ s3://kops-bjs/fileRepository/kubernetes-release/ --acl public-read
+    aws --profile bjs s3 sync s3://kubeupv2/ s3://kops-bjs/fileRepository/kubeupv2/ --acl=public-read
+    aws --profile bjs s3 sync s3://kubeupv2/kops/$KOPS_VERSION s3://kops-bjs/fileRepository/kops/$KOPS_VERSION --acl public-read 
+}
+
+mirror_kubernetes_release && \
+sync_release_to_s3 && \
+mirror_kubeupv2 && \
+mirror_fileRepo
