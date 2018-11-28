6a13f9d831c6c68792ba04219a06b97a4013ff26
Pahud Hsieh <pahudnet@gmail.com>
2018-11-28 21:18:33 +0800

    fetch ak/sk from parameter-store

--- mirror/codebuild/bin/bjs_ecr_auth.sh
+++ mirror/codebuild/bin/bjs_ecr_auth.sh
@@ -3,17 +3,12 @@
 # retrieve BJS credentials for ECR pusher
 
 
-echo $ak_kms_cipherblob | base64 -d > ak.blob
-
-ak=$(aws --region us-west-2 kms decrypt --ciphertext-blob fileb://ak.blob --output text --query Plaintext | base64 --decode)
-
-
-echo $sk_kms_cipherblob | base64 -d > sk.blob
-
-sk=$(aws --region us-west-2 kms decrypt --ciphertext-blob fileb://sk.blob --output text --query Plaintext | base64 --decode)
-
-rm -f ak.blob sk.blob
-
+# we should be able to retrieve ak/sk from parameter-store defined in buildspec.yml
+# echo $ak_kms_cipherblob | base64 -d > ak.blob
+# ak=$(aws --region us-west-2 kms decrypt --ciphertext-blob fileb://ak.blob --output text --query Plaintext | base64 --decode)
+# echo $sk_kms_cipherblob | base64 -d > sk.blob
+# sk=$(aws --region us-west-2 kms decrypt --ciphertext-blob fileb://sk.blob --output text --query Plaintext | base64 --decode)
+# rm -f ak.blob sk.blob
 #echo $ak
 #echo $sk
 
