edb2d09f7c79511a8d34949dcb4d9b518b49c8a9
Pahud <pahudnet@gmail.com>
2019-08-08 11:43:41 +0800

    minor update for display-remote-repos.sh https://github.com/nwcdlabs/kops-cn/issues/54#issuecomment-519350711

--- mirror/display-remote-repos.sh
+++ mirror/display-remote-repos.sh
@@ -3,7 +3,7 @@
 
 ECR_REGION='cn-north-1'
 ECR_DN="937788672844.dkr.ecr.${ECR_REGION}.amazonaws.com.cn"
-IMAGES_FILE_LIST='required-images.txt'
+IMAGES_FILE_LIST=${1-required-images.txt}
 NO_TRIM="gcr.io/istio-release/"
 
 function need_trim() {
